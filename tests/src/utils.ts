8d486128f494a2e63fdbb1d0171bf64d78dbb63f
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-02-20 18:09:45 +0100

    Fixed test app path

--- tests/src/utils.ts
+++ tests/src/utils.ts
@@ -1,5 +1,1 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-
-export const forumAppPath =
-  dirname(fileURLToPath(import.meta.url)) + "/../../workdir/forum.happ";
+export const forumAppPath = process.cwd() + "/../workdir/forum.happ";
