61ca25f28a3afe3e289b59ced0d0adee36707f31
Joshua Vial <joshua@enspiral.com>
2023-03-11 07:15:11 +1300

    exercise 2 step 2

--- dna/zomes/comments/src/lib.rs
+++ dna/zomes/comments/src/lib.rs
@@ -1,5 +1,31 @@
 use hdk::prelude::*;
 
+#[hdk_entry_helper]
+struct Comment {
+    comment: String
+}
+
+#[hdk_entry_defs]
+#[unit_enum(UnitEntryTypes)]
+enum EntryTypes {
+    Comment(Comment)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Input {
+    comment_on: ActionHash,
+    comment: String,
+}
+
+#[hdk_extern]
+fn create_comment(inp: Input) -> ExternResult<ActionHash> {
+    let comment = Comment {comment: inp.comment};
+    let action_hash = create_entry(EntryTypes::Comment(comment))?;
+
+    Ok(action_hash)
+}
+
+
 /**
  * Add your edits to the bottom of this file
  */
