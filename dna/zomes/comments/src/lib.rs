54684e2689999ad4197ecabc20b1a4d3eec77a35
Joshua Vial <joshua@enspiral.com>
2023-03-11 06:35:32 +1300

    delete wip

--- dna/zomes/comments/src/lib.rs
+++ dna/zomes/comments/src/lib.rs
@@ -1,16 +1,5 @@
 use hdk::prelude::*;
 
-#[hdk_entry_helper]
-struct Comment {
-    comment: String
-}
-
-#[hdk_entry_defs]
-#[unit_enum(UnitEntryTypes)]
-enum EntryTypes {
-    Comment(Comment)
-}
-
 /**
  * Add your edits to the bottom of this file
  */
